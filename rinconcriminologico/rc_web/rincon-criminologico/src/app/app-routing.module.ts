1b5550641bae13522dea8810473b3f1ec97d568e
Lucía Ruiz <lruimart@emeal.nttdata.com>
2022-10-11 17:57:54 +0200

    Created html code of forbidden and not-found components. Created shared module. Related routing modules.

--- rinconcriminologico/rc_web/rincon-criminologico/src/app/app-routing.module.ts
+++ rinconcriminologico/rc_web/rincon-criminologico/src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ForbiddenComponent } from './pages/commons/forbidden/forbidden.component';
+import { NotFoundComponent } from './pages/commons/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'externo'
+  },
+  {
+    path: 'externo',
+    loadChildren: () => import('./pages/external/external.module').then(m => m.ExternalModule),
+    canActivate: []
+  },
+  {
+    path: 'interno',
+    loadChildren: () => import('./pages/internal/internal.module').then(m => m.InternalModule),
+    // canActivate: [IsAuthGuard]
+    canActivate: []
+  },
+  {
+    path: 'no-encontrado',
+    component: NotFoundComponent
+  },
+  {
+    path: 'sin-permisos',
+    component: ForbiddenComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'no-encontrado'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- rinconcriminologico/rc_web/rincon-criminologico/src/app/pages/external/external-routing.module.ts
+++ rinconcriminologico/rc_web/rincon-criminologico/src/app/pages/external/external-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ExternalComponent } from './external.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ExternalComponent,
+    children: [
+      {
+        path: 'home',
+        loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
